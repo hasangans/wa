87aefe88f9ad9236b8d6b4160148437ffe65499c
riyoxdz <101568530+RiyoXdz@users.noreply.github.com>
2022-05-23 18:49:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281223918622'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281223918622'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281223918622' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'YT RIYO XDZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Minami-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'YT RIYO XDZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.youtube = 'https://youtube.com/channel/UCq2OIMRWymUW4ykJnfHp5PQ'
+global.ig = 'https://instagram.com/riyoxdz'
+global.mygc = 'https://chat.whatsapp.com/KglIECrtEuJ3yBPo0S5cDR'
 global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
-global.region = 'indonesia'
+global.email = 'mikumiki683@gmail.com'
+global.region = 'Ciamis, Jawa Barat, Indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Minami-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'YT RIYO XDZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -62,7 +62,7 @@ global.veloriy = fs.readFileSync('./image/chika.mp4')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.BgWelcLeav = 'https://telegra.ph/file/4cf7b1b975fd1748a4a5c.jpg'
 
 //—————「 Set Random Image Menu 」—————//
 //gausah di apa² in!
@@ -83,15 +83,15 @@ global.sp = '⭔'
 //terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
 global.mess = {
 success: '_Done._',
-admin: '_Fitur Khusus Admin Group!_',
-botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
-owner: '_Fitur Khusus Owner Bot_',
-group: '_Fitur Digunakan Hanya Untuk Group!_',
+admin: '_Fitur Khusus Admin Group Goblok Setan Babi!_',
+botAdmin: '_Jadiin Admin Dulu Lah Bot Nya,,Dasar Tolol!_',
+owner: '_Fitur Khusus RIYO XDZ_',
+group: '_Fitur Digunakan Hanya Untuk Group Bwang!_',
 private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
 bot: '_Fitur Khusus Pengguna Nomor Bot_',
-wait: '_Sedang Di Proses..._',
+wait: '_Sabar Tolol Loading..._',
 error: '_Fitur Sedang Error!_',
-endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12,,Hahah Mampus_',
 }
 
 //—————「 Batas Akhir 」—————//
