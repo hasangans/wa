22e3ea24eac5027fe9c0ab0aa337a7b9f2cc8955
hasangans <60222529+hasangans@users.noreply.github.com>
2022-05-27 18:49:33 +0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,105 @@
+/*
+› Create By @RIY
+› Base Ori @DikaArdnt
+› Kalau mau tambahin fitur di index.js
+› Edit text menu di file language › help.js
+*/
+
+//gausah di apa² in!
+const fs = require('fs')
+const chalk = require('chalk')
+
+//—————「 Website Apikey 」—————//
+global.APIs = {
+//gausah di apa² in!
+zenz: 'https://zenzapis.xyz',
+}
+
+//—————「 Website Apikey 」—————//
+global.APIKeys = {
+//gausah di apa² in, udah free apikey :)
+'https://zenzapis.xyz': 'BagasPrdn',
+}
+
+//—————「 Setting Owner 」—————//
+global.owner = ['6285654783538'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285654783538'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285654783538' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'HasanGans' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Hasan-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Hasan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+
+//—————「 Set Kebutuhan Button & Kontak 」—————//
+//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
+global.youtube = 'https://youtube.com/channel/UCq2OIMRWymUW4ykJnfHp5PQ'
+global.ig = 'https://instagram.com/riyoxdz'
+global.mygc = 'https://chat.whatsapp.com/KglIECrtEuJ3yBPo0S5cDR'
+global.myweb = 'https://s.id/RiyAPIs'
+global.email = 'mikumiki683@gmail.com'
+global.region = 'Banjarmasin, Kalimantan, Indonesia'
+
+//—————「 Set Wm 」—————//
+global.packname = '© Hasan-md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Hasan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+
+//—————「 Set Nama Session 」—————//
+//gausah di apa² in!
+global.sessionName = 'session'
+
+//—————「 Set Limit 」—————//
+//terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
+global.limitawal = {
+premium: "Infinity",
+free: 15,
+}
+
+//—————「 Set Image 」—————//
+//terserah mau ubah apa nggak, menurut gua jangan di ubah!
+global.thumb = fs.readFileSync('./image/chika.jpg')
+global.veloriy = fs.readFileSync('./image/chika.mp4')
+
+//—————「 Set Image Welcome 」—————//
+//terserah mau ubah apa nggak, menurut gua jangan di ubah!
+global.BgWelcLeav = 'https://telegra.ph/file/4cf7b1b975fd1748a4a5c.jpg'
+
+//—————「 Set Random Image Menu 」—————//
+//gausah di apa² in!
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//—————「 Set Prefix 」—————//
+//gausah di apa² in!
+global.prefa = ['','!','.','🐦','🐤','🗿']
+
+//—————「 Set Simbol 」—————//
+//terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
+global.sp = '⭔'
+
+//—————「 Set Message 」—————//
+//terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
+global.mess = {
+success: '_Done._',
+admin: '_Fitur Khusus Admin Group i!_',
+botAdmin: '_Jadiin Admin Dulu Lah Bot Nya,,Dasar Tolol!_',
+owner: '_Fitur Khusus bangHasan',
+group: '_Fitur Digunakan Hanya Untuk Group Bwang!_',
+private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
+bot: '_Fitur Khusus Pengguna Nomor Bot_',
+wait: '_Sabar Tolol Loading..._',
+error: '_Fitur Sedang Error!_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12,,Hahah Mampus_',
+}
+
+//—————「 Batas Akhir 」—————//
+//gausah si apa² in!
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
